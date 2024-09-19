cb2a045547336cc2367b7f561e9485e21c6eba9a
Sarah John <sarahjohn678@gmail.com>
2024-09-18 21:59:04 -0400

    array set up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,32 @@
 let projects = [
-    {
-        
-    },{}
-
+  {
+    name: 'Food Finder',
+    shortDescription: 'A Geographical Interface System built on C+',
+    img: 'assets/img/google-maps.webp',
+    details: [
+      'Tools: C++, OpenStreetMap API, GTK',
+      'Worked in a team of 3 to build a largescale interactive mapping software (similar to Google Maps) using Agile project management practices.',
+      'Used OpenStreetMap API, C++, and GTK graphics library to build a user friendly map.'
+    ]
+  },
+  {
+    name: 'Personal Web Site',
+    shortDescription: 'A simple personal website highlighting my experience and passions.',
+    img: '/assets/img/project-blog-logo.jpg',
+    details: [
+      'Tools: HTML, CSS, Bootstrap, Angular, Typescript',
+      'A simple and easy to use website highlighting my education, acheivements, and extracurriculars.',
+      'Built from scratch using Angular framework and skills in HTML, CSS, and TypeScript'
+    ]
+  },
+  {
+    name: 'Capstone Project',
+    shortDescription: 'A user-friendly website for project managers to plan, schedule, and manage software projects.',
+    details: [
+      'Tools: Unknown Yet',
+      'A simple and easy to use website for project managers',
+      'Implements a Deep Reinforcement Algorithm.'
+    ]
+  }
+  
 ];
